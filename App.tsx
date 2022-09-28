3bc3bc1c3425c2515f0a16ab8348e8166db3defd
Alessandro Diez <alessandro.diez@email.com>
2022-09-28 12:11:13 +0200

    scroll to index on rotation change

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   FlatList,
   SafeAreaView,
@@ -11,14 +11,29 @@ import {
 
 const App = () => {
   const {width, height} = useWindowDimensions();
+  const [page, setPage] = useState(1);
+  const flatListRef = useRef<FlatList>(null);
+
+  const isPortrait = height > width;
+  useEffect(() => {
+    flatListRef.current?.scrollToIndex({index: page - 1, animated: false});
+  }, [isPortrait, page]);
 
   return (
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <FlatList
+          ref={flatListRef}
           data={Array.from({length: 20})}
           horizontal
           pagingEnabled
+          onMomentumScrollEnd={e => {
+            const index = Math.round(
+              e.nativeEvent.contentOffset.x /
+                e.nativeEvent.layoutMeasurement.width,
+            );
+            setPage(index + 1);
+          }}
           getItemLayout={(_data, index) => {
             return {
               index,
